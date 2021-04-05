016d434e9e357950703e498567f7e67baabbfa1f
Ashton Huxtable <aehuxtable@gmail.com>
2021-04-04 20:05:52 -0600

    Merge displayMiniCover and SaveCover functions to fix bug

--- src/main.js
+++ src/main.js
@@ -3,18 +3,18 @@ var cover = document.querySelector('.cover-image')
 var title = document.querySelector('.cover-title')
 var descriptor1 = document.querySelector('.tagline-1')
 var descriptor2 = document.querySelector('.tagline-2')
-var randomCoverButton = document.querySelector('.random-cover-button')
 var currentCover = new Cover(covers[getRandomIndex(covers)], titles[getRandomIndex(titles)], descriptors[getRandomIndex(descriptors)], descriptors[getRandomIndex(descriptors)])
 
+var randomCoverButton = document.querySelector('.random-cover-button')
 var newCoverButton = document.querySelector('.make-new-button')
-var formView = document.querySelector('.view.form-view')
-var homeView = document.querySelector('.view.home-view')
 var saveNewButton = document.querySelector('.save-cover-button')
 var homeButton =document.querySelector('.home-button')
-
 var savedButton = document.querySelector('.view-saved-button')
+
+var formView = document.querySelector('.view.form-view')
+var homeView = document.querySelector('.view.home-view')
 var savedCoversView = document.querySelector('.view.saved-view')
-var savedCoversSection = document.querySelector('.saved-covers-section')
+
 
 var formCover = document.querySelector('#cover')
 var formTitle = document.querySelector('#title')
@@ -22,7 +22,8 @@ var formDescriptor1 = document.querySelector('#descriptor1')
 var formDescriptor2 = document.querySelector('#descriptor2')
 var myBookButton = document.querySelector('.create-new-book-button')
 
-// We've provided a few variables below
+var savedCoversSection = document.querySelector('.saved-covers-section')
+
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
@@ -100,20 +101,18 @@ function saveMyBook(event) {
 function saveCover(){
   if (savedCovers.includes(currentCover) === false) {
     savedCovers.push(currentCover)
+    savedCoversSection.innerHTML += `
+      <section class="mini-cover" id="${currentCover.id}">
+        <img class="cover-image" src="${currentCover.cover}">
+        <h2 class="cover-title">${currentCover.title}</h2>
+        <h3 class="tagline">A tale of <span class="tagline-1">${currentCover.tagline1}</span> and <span class="tagline-2">${currentCover.tagline2}</span></h3>
+        <img class="price-tag" src="./assets/price.png">
+        <img class="overlay" src="./assets/overlay.png">
+      </section>`
   }
-  displayMiniCovers()
 }
 
-function displayMiniCovers() {
-  savedCoversSection.innerHTML += `
-    <section class="mini-cover" id="${currentCover.id}">
-      <img class="cover-image" src="${currentCover.cover}">
-      <h2 class="cover-title">${currentCover.title}</h2>
-      <h3 class="tagline">A tale of <span class="tagline-1">${currentCover.tagline1}</span> and <span class="tagline-2">${currentCover.tagline2}</span></h3>
-      <img class="price-tag" src="./assets/price.png">
-      <img class="overlay" src="./assets/overlay.png">
-    </section>`
-}
+
 
 function deleteSavedCover() {
   var clickedCover = event.target.closest(".mini-cover")
@@ -122,5 +121,5 @@ function deleteSavedCover() {
       savedCovers.splice(i, 1)
     }
   }
-  displayMiniCovers()
+  viewSavedCovers()
 }
